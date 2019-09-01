229e1eabd9796d6551337487909c928f9228753c
soderlind <per@soderlind.no>
2019-09-02 00:06:41 +0200

    find mysql

--- wp-cli/entrypoint.sh
+++ wp-cli/entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-if ! $(sh -c "wp --allow-root core is-installed"); then
-    sh -c "wp --allow-root core install"
-fi
-sh -c "wp --allow-root --path=/usr/src/wordpress/ $*"
+# if ! $(sh -c "wp --allow-root core is-installed"); then
+#     sh -c "wp --allow-root core install"
+# fi
+# sh -c "wp --allow-root --path=/usr/src/wordpress/ $*"
 
-# sh -c "find / -name wp\*"
+sh -c "find / -name mysql\*"
