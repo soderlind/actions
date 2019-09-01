260fdc4a8c0694e41d79db83f2bdff040aa07238
soderlind <per@soderlind.no>
2019-09-01 22:36:30 +0200

    wp core install

--- wp-cli/entrypoint.sh
+++ wp-cli/entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-# sh -c "wp --allow-root --path='/github/home/var/www/html' $*"
+if ! $(sh -c "wp --allow-root core is-installed"); then
+    sh -c "wp --allow-root core install"
+fi
+sh -c "wp --allow-root --path=/usr/src/wordpress/ $*"
 
-sh -c "find / -name wp\*"
+# sh -c "find / -name wp\*"
